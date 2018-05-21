3520794e91180d0842ad885a04b6d1458f68885a
Isaac Gravestock <Isaac.Gravestock@usz.ch>
2018-05-21 10:40:16 +0200

    new sim and function

---
+++ pkg/StudyPrior/Examples/RandModel75.R
@@ -0,0 +1,129 @@
+# RandomModelSim
+
+# New simulation
+
+
+library(parallel)
+library(StudyPrior)
+inla.setOption(num.threads=2)
+
+
+
+NH <- 100
+NN <- 75
+NT <- 200
+
+
+gen.models <- function(I){
+  
+  print(I)
+  
+  set.seed(2019)
+  rnorm(500*I)
+  
+  N <- 5
+  nh <- rep(NH,N)
+  
+  z <- rnorm(N, 0.65, 0.05)
+  
+  xh <- mapply(rbinom, size=nh, n=1, prob=z)
+  
+  binom.PP.EB(xh, nh, N=NN, mix=TRUE) -> a
+  binom.PP.EB.Sep(xh, nh, N=NN, mix=TRUE) -> b
+  binom.PP.EB(sum(xh), sum(nh), N=NN, mix=TRUE) -> c
+  
+  
+  lapply(a, function(A) create.mixture.prior("beta",
+                                             pars = rbind(attr(A,"pars"),c(1,1)),
+                                             weights = c(0.5*attr(A,"weights"),0.5))) -> d
+  
+  binom.PP.EB(xh, nh, N=NN, mix=TRUE, max.dn = 200-75) -> e
+  
+  binom.PP.EB(xh, nh, N=NN, mix=TRUE, max.dn = 50) -> f
+  
+  # binom.PP.FB.COR(xh, nh, d.prior.cor = 0, mix=TRUE) -> d
+  # binom.PP.FB.COR(xh, nh, d.prior.cor = .5, mix=TRUE) -> e
+  # binom.PP.FB.COR(xh, nh, d.prior.cor = 1, mix=TRUE) -> f
+  
+  binom.MAP.FB(xh, nh) -> map.1
+  conj.approx(distr = map.1,type = 'beta', max.degree = 10) -> g
+  
+  create.mixture.prior("beta",
+                       pars = rbind(attr(g,"pars"),c(1,1)),
+                       weights = c(0.5*attr(g,"weights"),0.5)) -> h
+  
+  create.mixture.prior("beta",pars=matrix(c(1,1),ncol=2))->k
+  
+  list.prior <- list(a,b,c,d,e,f,g,h,k)
+  
+  lapply(list.prior, function(priors){
+    if(length(priors)>1) {
+      lapply(0:NN, function(x) posterior.mixture.prior(xs=x, n=NN, mixture.prior = priors[[x+1]]))  
+    }else{
+      lapply(0:NN, function(x) posterior.mixture.prior(xs=x, n=NN, mixture.prior = priors))  
+    }
+  }) -> list.posterior
+ 
+  
+  save(file=paste0('models_',formatC(I, width=4, flag="0"),'.rda'),
+       list.prior, list.posterior,xh,nh,  NH, NN, NT )
+  
+}
+
+
+
+
+
+calc.oc <- function(I){
+  try( {load(file=paste0('models_',formatC(I, width=4, flag="0"),'.rda'))
+  
+    
+   ess <- lapply(list.posterior, function(p) sapply(p, ess.mixture.prior))
+    
+    mse <- lapply(list.posterior,
+                  function(pr) calc.MSE.mean(posterior=pr, prob.range=c(0,1), length = 100,  n.binom=NN))
+    
+  
+    bias <- lapply(list.posterior,
+                   function(pr) calc.bias(posterior = pr, prob.range=c(0,1), length = 100, n.binom=NN))
+        
+    SIGMAT <- lapply(list.posterior, function(pr) sig.matrix(posterior = pr, n.control=NN, n.treatment = NT,
+                                                   check.xt=00:NT, check.xs=0:NN,
+                                                   level=0.975))
+    
+    
+    pow <- lapply(SIGMAT,
+                    function(S) calc.power(sig.mat=S, n.binom.control = NN, n.binom.treatment = NT,
+                                           prob.range = c(0,0.85), length =200, treatment.difference=0.12))
+    
+    lr <- lapply(SIGMAT,
+                    function(S) calc.power(sig.mat=S, n.binom.control = NN, n.binom.treatment = NT,
+                                           prob.range = c(0,0.85), length =200, treatment.difference=0.12, LR=TRUE))
+    
+    t1e <- lapply(SIGMAT,
+                    function(S) calc.power(sig.mat=S, n.binom.control = NN, n.binom.treatment = NT, 
+                                           prob.range = c(0,0.85), length = 200, treatment.difference = 0))
+ 
+    
+    save(file=paste0('oc_',formatC(I, width=4, flag="0"),'.rda'),
+         ess,mse, bias, SIGMAT, pow, t1e,lr, xh,nh,  NH, NN, NT )
+  })
+}
+
+
+mclapply(1:1000, gen.models, mc.cores=20)
+# mclapply(1:1000, calc.oc, mc.cores=30)
+  
+
+TF2 <- rep(FALSE,1000)
+for(I in 1:1000) TF2[I] <-  !file.exists(paste0('models_',formatC(I, width=4, flag="0"),'.rda'))
+recalc2 <- which(TF2)
+mclapply(recalc2, gen.models, mc.cores=20)
+#   
+
+
+TF3 <- rep(FALSE,1000)
+for(I in 1:1000) TF3[I] <-  !file.exists(paste0('oc_',formatC(I, width=4, flag="0"),'.rda'))
+recalc3 <- which(TF3)
+mclapply(recalc3, calc.oc, mc.cores=39, mc.preschedule = FALSE)
+
--- pkg/StudyPrior/R/binom.PP.EB.R
+++ pkg/StudyPrior/R/binom.PP.EB.R
@@ -8,13 +8,14 @@
 #' @param mc.cores number of cores for parallel
 #' @param p.prior.a shape1 parameter of initial beta prior for successes
 #' @param p.prior.b shape2 parameter of initial beta prior for successes 
+#' @param max.dn Set a limit on the sum of the weights*n
 #' @param mix if TRUE return a mixture.prior object otherwise the function
 #'
 #' @return Either a list of mixture.prior objects which can be evaluated with eval.mixture.prior or a density function of the probability parmater p given X.
 #' @export
 #'
 #'
-binom.PP.EB <- function(x, n, X, N, verbose=FALSE, mc.cores=1, p.prior.a=1, p.prior.b=1, mix=FALSE){
+binom.PP.EB <- function(x, n, X, N, verbose=FALSE, mc.cores=1, p.prior.a=1, p.prior.b=1, max.dn = FALSE, mix=FALSE){
   #if X isn't specified we calculate it for all, set flag too
   if(missing(X)) {
     X <- 0:N
@@ -46,9 +47,11 @@ binom.PP.EB <- function(x, n, X, N, verbose=FALSE, mc.cores=1, p.prior.a=1, p.pr
 
     if(opd$convergence!=0) print(opd)
 
+    
+    
     d <- opd$par
 
-
+    if(max.dn & sum(d*n) > max.dn ) d <- d * max.dn/sum(d*n)
 
     return(d)
   })
